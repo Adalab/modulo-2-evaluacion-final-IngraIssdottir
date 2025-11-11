3daa6ed90e65ceb44a83415510847b5b43692cbc
IngraIssdottir <ingra.issdottir@yahoo.es>
2025-11-11 20:06:15 +0100

    rendering cards

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const productsList = document.querySelector('.js_products_list');
+
+const apiURL = 'https://raw.githubusercontent.com/Adalab/resources/master/apis/products.json';
+
+let products = [];
+
+
+function renderOneProduct(oneProduct) {
+    const html = `<article class="product_card">
+          <img class="product_image" src="${oneProduct.image}" alt="">
+          <h3 class="product_name">${oneProduct.title}</h3>
+          <p class="Product_price">${oneProduct.price}</p>
+          <button class="buy_button">Comprar</button>
+        </article>`;
+
+  return html;
+}
+
+function renderProducts(products) {
+    let html = '';
+
+    for( const oneProduct of products)  {
+        html += renderOneProduct(oneProduct);
+    }
+
+    productsList.innerHTML = html;
+}
+
+
+fetch(apiURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data)
+    products = data;
+
+    renderProducts(products);
+
+});
